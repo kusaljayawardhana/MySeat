36fe0935221ee0235944a8203ed5ff920bfad632
kusaljayawardhana <e20179@eng.pdn.ac.lk>
2026-02-27 00:41:58 +0530

    feat: Initialize Gradle wrapper and project structure

---
+++ myseat/src/main/java/com/kusal/myseat/MyseatApplication.java
@@ -0,0 +1,13 @@
+package com.kusal.myseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyseatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyseatApplication.class, args);
+	}
+
+}
